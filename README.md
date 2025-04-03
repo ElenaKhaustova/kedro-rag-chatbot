ad8e2c0a66ab10fab0f431a067f18385d64c0255
ElenaKhaustova <ymax70rus@gmail.com>
2025-04-03 14:16:36 +0100

    Added project structure
    Signed-off-by: ElenaKhaustova <ymax70rus@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # kedro-rag-chatbot
 RAG-based Chatbot with Kedro
+
+## Overview
+
+## How to install dependencies
+
+To install dependencies, run:
+
+```
+pip install -r requirements.txt
+```
+
+## How to run your Kedro pipeline
+
+You can run your Kedro project with:
+
+```
+kedro run
+```
---
+++ src/kedro_rag_chatbot/pipelines/create_vector_store/nodes.py
@@ -0,0 +1,61 @@
+import logging
+from typing import Any, Callable
+
+from deeplake.core.vectorstore import VectorStore
+import numpy as np
+from sklearn.feature_extraction.text import HashingVectorizer
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_texts(dialog: dict[str, Any]) -> str:
+    lines = []
+    if "user" in dialog and "text" in dialog:
+        lines.append(f"{dialog['user']}: {dialog['text']}")
+    if "replies" in dialog:
+        for reply in dialog["replies"]:
+            if "user" in reply and "text" in reply:
+                lines.append(f"{reply['user']}: {reply['text']}")
+    return "\n\n".join(lines)
+
+
+def format_dialogs(dialogs: dict[str, dict]) -> dict[str, str]:
+    texts = {}
+    for dialog_name, dialog in dialogs.items():
+        text = get_texts(dialog)
+        if text:
+            texts[dialog_name] = text
+            logger.info(f"Formatted dialog {dialog_name}")
+        else:
+            logger.warning(f"Empty dialog {dialog_name}")
+    return texts
+
+
+def create_embedding_function() -> Callable:
+    def embedding_function(texts: list[str], embeddings_size: int = 2**16) -> list:
+        if isinstance(texts, str):
+            texts = [texts]
+        vectorizer = HashingVectorizer(n_features=embeddings_size, dtype=np.float32)
+        X = vectorizer.fit_transform(texts)
+
+        return [vector.toarray().flatten() for vector in X]
+
+    return embedding_function
+
+
+def create_vector_store(
+    vector_store: VectorStore,
+    formatted_dialogs: dict[str, str],
+    embedding_function: Callable,
+    embeddings_size: int,
+) -> VectorStore:
+    texts = [dialog for dialog in formatted_dialogs.values()]
+    embeddings = embedding_function(texts, embeddings_size)
+    vector_store.add(
+        text=texts,
+        embedding=embeddings,
+        metadata=[{"dialog_id": dialog_id} for dialog_id in formatted_dialogs.keys()],
+    )
+
+    return vector_store
